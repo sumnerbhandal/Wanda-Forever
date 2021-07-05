2f1752ead3628784caf60ed86e52b45f9c3d6358
Sumner Bhandal <34420026+sumnerbhandal@users.noreply.github.com>
2021-07-05 12:41:36 +0100

    Emptied out

--- src/login/login.js
+++ src/login/login.js
@@ -1,47 +1,11 @@
 import React from "react";
-import TextInput from "../_input/textInput/textInput";
-import Button from "../_input/button/button";
-import { Link } from "react-router-dom/index";
 import "./styles.scss";
 
 const LoginPage = (props) => {
   return (
     <div className="login-page">
-      <div className="left">
-        <img
-          alt="Pubic Demo"
-          className="login-hero"
-          src="https://uploads-ssl.webflow.com/5dc98e8b04adb0e7091d2920/5dc98e8b04adb0146c1d2ad1_Asset%2012.svg"
-        />
-      </div>
-      <div className="right">
-        {/* <form>
-          <div className="robin-logo">
-            <img
-              alt="Robin Logo"
-              src="https://portal.robinai.co.uk/images/robin-logo.svg"
-            />
-          </div>
-          <TextInput
-            label="Username"
-            type="text"
-            name="email"
-            placeholder="joe.bloggs@lawsociety.com"
-          />
-          <TextInput
-            label="Password"
-            type="password"
-            name="password"
-            placeholder="Password"
-          />
-          <div className="submit-reset">
-            <Link to="./contracts" tabIndex="0">
-              <Button variant="primary" type="submit" label="Login ->" />
-            </Link>
-            <Button variant="text" type="button" label="Forgot Password?" />
-          </div>
-        </form> */}
-      </div>
+      <div className="left"></div>
+      <div className="right"></div>
     </div>
   );
 };

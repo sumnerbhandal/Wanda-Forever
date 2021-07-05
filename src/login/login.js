8ef330ff561866a393a79e8e761ceb2a70b57b1b
Sumner Bhandal <34420026+sumnerbhandal@users.noreply.github.com>
2021-07-05 12:51:57 +0100

    Removal of nested components

--- src/login/login.js
+++ src/login/login.js
@@ -1,6 +1,4 @@
 import React from "react";
-import TextInput from "../_input/textInput/textInput";
-import Button from "../_input/button/button";
 import { Link } from "react-router-dom/index";
 import "./styles.scss";
 
@@ -22,23 +20,9 @@ const LoginPage = (props) => {
               src="https://portal.robinai.co.uk/images/robin-logo.svg"
             />
           </div>
-          <TextInput
-            label="Username"
-            type="text"
-            name="email"
-            placeholder="joe.bloggs@lawsociety.com"
-          />
-          <TextInput
-            label="Password"
-            type="password"
-            name="password"
-            placeholder="Password"
-          />
+
           <div className="submit-reset">
-            <Link to="./contracts" tabIndex="0">
-              <Button variant="primary" type="submit" label="Login ->" />
-            </Link>
-            <Button variant="text" type="button" label="Forgot Password?" />
+            <Link to="./contracts" tabIndex="0"></Link>
           </div>
         </form>
       </div>

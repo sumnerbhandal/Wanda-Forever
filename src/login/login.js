250e933acf5902fbbeb2e996e91d4fb83b444cad
Sumner Bhandal <34420026+sumnerbhandal@users.noreply.github.com>
2021-07-05 12:47:36 +0100

    Links?

--- src/login/login.js
+++ src/login/login.js
@@ -1,26 +1,26 @@
 import React from "react";
 import TextInput from "../_input/textInput/textInput";
 import Button from "../_input/button/button";
-import { Link } from "react-router-dom/index";
+// import { Link } from "react-router-dom/index";
 import "./styles.scss";
 
 const LoginPage = (props) => {
   return (
     <div className="login-page">
       <div className="left">
-        {/* <img
+        <img
           alt="Pubic Demo"
           className="login-hero"
           src="https://uploads-ssl.webflow.com/5dc98e8b04adb0e7091d2920/5dc98e8b04adb0146c1d2ad1_Asset%2012.svg"
-        /> */}
+        />
       </div>
       <div className="right">
         <form>
           <div className="robin-logo">
-            {/* <img
+            <img
               alt="Robin Logo"
               src="https://portal.robinai.co.uk/images/robin-logo.svg"
-            /> */}
+            />
           </div>
           <TextInput
             label="Username"
@@ -35,9 +35,9 @@ const LoginPage = (props) => {
             placeholder="Password"
           />
           <div className="submit-reset">
-            <Link to="./contracts" tabIndex="0">
-              <Button variant="primary" type="submit" label="Login ->" />
-            </Link>
+            {/* <Link to="./contracts" tabIndex="0"> */}
+            <Button variant="primary" type="submit" label="Login ->" />
+            {/* </Link> */}
             <Button variant="text" type="button" label="Forgot Password?" />
           </div>
         </form>

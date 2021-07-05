4e538f70a7edba1b52bac2fb23481f0744caae86
Sumner Bhandal <34420026+sumnerbhandal@users.noreply.github.com>
2021-07-05 12:10:01 +0100

    Link directly to externally hosted images to speed this up

--- src/login/login.js
+++ src/login/login.js
@@ -1,8 +1,8 @@
 import React from "react";
 import TextInput from "../_input/textInput/textInput";
 import Button from "../_input/button/button";
-import loginImage from "./img/login.svg";
-import robinLogo from "./img/robin-logo.svg";
+// import loginImage from "./img/login.svg";
+// import robinLogo from "./img/robin-logo.svg";
 import { Link } from "react-router-dom/index";
 import "./styles.scss";
 
@@ -10,12 +10,15 @@ const LoginPage = (props) => {
   return (
     <div className="login-page">
       <div className="left">
-        <img className="login-hero" src={loginImage} />
+        <img
+          className="login-hero"
+          src="https://uploads-ssl.webflow.com/5dc98e8b04adb0e7091d2920/5dc98e8b04adb0146c1d2ad1_Asset%2012.svg"
+        />
       </div>
       <div className="right">
         <form>
           <div className="robin-logo">
-            <img src={robinLogo} />
+            <img src="https://portal.robinai.co.uk/images/robin-logo.svg" />
           </div>
           <TextInput
             label="Username"

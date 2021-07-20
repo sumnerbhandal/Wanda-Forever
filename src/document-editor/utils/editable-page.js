ad04ae545a597a9a84f5d61178f4e1f47a4ec07e
Sumner Bhandal <34420026+sumnerbhandal@users.noreply.github.com>
2021-07-20 09:42:07 +0100

    Maybe this works in production

--- src/document-editor/utils/editable-page.js
+++ src/document-editor/utils/editable-page.js
@@ -1,5 +1,4 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import React, { Component, useEffect, useState } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 import "../styles.scss";
@@ -12,12 +11,12 @@ import GenericNda from "../../_api/nda/nda.json";
 
 const initialBlock = { id: uid(), html: "example", tag: "p" };
 
-// // fake data generator
-// const getItems = (count) =>
-//   Array.from({ length: count }, (v, k) => k).map((k) => ({
-//     id: `item-${k}`,
-//     content: `item ${k}`
-//   }));
+// fake data generator
+const getItems = (count) =>
+  Array.from({ length: count }, (v, k) => k).map((k) => ({
+    id: `item-${k}`,
+    content: `item ${k}`
+  }));
 
 // a little function to help us with reordering the result
 const reorder = (list, startIndex, endIndex) => {
@@ -28,12 +27,9 @@ const reorder = (list, startIndex, endIndex) => {
   return result;
 };
 
-const grid = 8;
-
 const getItemStyle = (isDragging, draggableStyle) => ({
   // some basic styles to make the items look a bit nicer
   userSelect: "none",
-  // padding: grid * 2,
 
   // change background colour if dragging
   background: isDragging ? "#e6e6e6" : null,
@@ -46,23 +42,24 @@ const getItemStyle = (isDragging, draggableStyle) => ({
 //   background: isDraggingOver ? "lightblue" : "lightgrey"
 // });
 
-class EditablePage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.updatePageHandler = this.updatePageHandler.bind(this);
-    this.addBlockHandler = this.addBlockHandler.bind(this);
-    this.deleteBlockHandler = this.deleteBlockHandler.bind(this);
-    this.onDragEnd = this.onDragEnd.bind(this);
-    this.state = { blocks: [initialBlock] };
-    this.onDragEnd = this.onDragEnd.bind(this);
-  }
+const EditablePage = (props) => {
+  const [blocks, setBlocks] = useState([initialBlock]);
 
-  componentDidMount() {
-    this.setState({ blocks: GenericNda.NDA });
-  }
+  useEffect(() => {
+    setBlocks(GenericNda.NDA);
+  }, []);
 
-  updatePageHandler(updatedBlock) {
-    const blocks = this.state.blocks;
+  // componentDidMount() {
+  //   this.setState({ blocks: GenericNda.NDA });
+  //   console.log();
+  // }
+
+  // componentDidUpdate() {
+  //   const length = this.state.blocks.length;
+  //   // this.setState({ items: getItems(length) });
+  // }
+
+  function updatePageHandler(updatedBlock) {
     const index = blocks.map((b) => b.id).indexOf(updatedBlock.id);
     const updatedBlocks = [...blocks];
     updatedBlocks[index] = {
@@ -70,43 +67,41 @@ class EditablePage extends React.Component {
       tag: updatedBlock.tag,
       html: updatedBlock.html
     };
-    this.setState({ blocks: updatedBlocks });
+    setBlocks(updatedBlocks);
   }
 
-  addBlockHandler(e) {
+  function addBlockHandler(e) {
     const newBlock = { id: uid(), html: "", tag: "p" };
-    const blocks = this.state.blocks;
     const index = blocks.map((b) => b.id).indexOf(e.id);
     const updatedBlocks = [...blocks];
     updatedBlocks.splice(index + 1, 0, newBlock);
-    this.setState({ blocks: updatedBlocks }, () => {
-      e.ref.parentNode.nextElementSibling.firstChild.focus();
-    });
+    setBlocks(updatedBlocks);
+    console.log(e.ref.parentNode.nextElementSibling.firstChild);
+    e.ref.parentNode.nextElementSibling.firstChild.focus();
   }
 
-  deleteBlockHandler(e) {
+  function deleteBlockHandler(e) {
     // Only delete the block, if there is a preceding one
     const previousBlock = e.ref.parentNode.previousElementSibling.firstChild;
     if (previousBlock) {
-      const blocks = this.state.blocks;
       const index = blocks.map((b) => b.id).indexOf(e.id);
       const updatedBlocks = [...blocks];
       updatedBlocks.splice(index, 1);
-      this.setState({ blocks: updatedBlocks }, () => {
+      setBlocks(updatedBlocks, () => {
         setCaretToEnd(previousBlock);
         previousBlock.focus();
       });
     }
   }
 
-  onDragEnd(result) {
+  function onDragEnd(result) {
     // dropped outside the list
     if (!result.destination) {
       return;
     }
 
     const items = reorder(
-      this.state.blocks,
+      blocks,
       result.source.index,
       result.destination.index
     );
@@ -116,57 +111,55 @@ class EditablePage extends React.Component {
     });
   }
 
-  render() {
-    return (
-      <div className="contract">
-        <DragDropContext onDragEnd={this.onDragEnd}>
-          <Droppable droppableId="droppable">
-            {(provided, snapshot) => (
-              <div
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-                // style={getListStyle(snapshot.isDraggingOver)}
-              >
-                {this.state.blocks.map((block, key) => (
-                  <Draggable
-                    className="list-reorder"
-                    key={block.id}
-                    draggableId={block.id}
-                    index={key}
-                    tabIndex="-1"
-                  >
-                    {(provided, snapshot) => (
-                      <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        style={getItemStyle(
-                          snapshot.isDragging,
-                          provided.draggableProps.style
-                        )}
-                        tabIndex="-1"
-                      >
-                        <EditableBlock
-                          key={key}
-                          id={block.id}
-                          tag={block.tag}
-                          html={block.html}
-                          updatePage={this.updatePageHandler}
-                          addBlock={this.addBlockHandler}
-                          deleteBlock={this.deleteBlockHandler}
-                        />
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="contract">
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              // style={getListStyle(snapshot.isDraggingOver)}
+            >
+              {blocks.map((block, key) => (
+                <Draggable
+                  className="list-reorder"
+                  key={block.id}
+                  draggableId={block.id}
+                  index={key}
+                  tabIndex="-1"
+                >
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={getItemStyle(
+                        snapshot.isDragging,
+                        provided.draggableProps.style
+                      )}
+                      tabIndex="-1"
+                    >
+                      <EditableBlock
+                        key={key}
+                        id={block.id}
+                        tag={block.tag}
+                        html={block.html}
+                        updatePage={updatePageHandler}
+                        addBlock={addBlockHandler}
+                        deleteBlock={deleteBlockHandler}
+                      />
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
+};
 
 export default EditablePage;

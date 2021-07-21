41f24b54a22e611522cbfc0cc5365de6bb61ca38
Sumner Bhandal <34420026+sumnerbhandal@users.noreply.github.com>
2021-07-21 10:51:37 +0100

    Styles pushed

--- src/canvas/canvas.js
+++ src/canvas/canvas.js
@@ -11,12 +11,16 @@ const Canvas = (props) => {
           <span className="material-icons">clear</span>
         </button>
         <div className="section">
-          <p className="logo">
-            <strong>Project Wanda</strong>
-          </p>
+          <div className="robin-logo">
+            <img
+              alt="Robin Logo"
+              src="https://portal.robinai.co.uk/images/robin-logo.svg"
+            />
+          </div>
         </div>
         <div className="section">
-          <p className="logo">
+          <p className="user">
+            <span class="material-icons">account_circle</span>
             <strong>{props.username}</strong>
           </p>
         </div>

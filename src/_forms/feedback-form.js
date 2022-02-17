e67c14331e862b28cad38a132a693d862c16120d
Sumner Bhandal <34420026+sumnerbhandal@users.noreply.github.com>
2022-02-17 17:03:56 +0000

    Multiple form submission prevented

--- src/_forms/feedback-form.js
+++ src/_forms/feedback-form.js
@@ -20,6 +20,7 @@ const submitFeedback = (
 export const FeedbackForm = () => {
   const form = useRef();
   const [popupOpen, setPopUpOpen] = useToggle();
+  const [formSubmitted, setFormSubmitted] = useState(false);
   const pageUrl = window.location.href;
 
   const sendEmail = (e) => {
@@ -35,6 +36,7 @@ export const FeedbackForm = () => {
       .then(
         (result) => {
           console.log(result.text);
+          setFormSubmitted(true);
         },
         (error) => {
           console.log(error.text);
@@ -58,43 +60,58 @@ export const FeedbackForm = () => {
             src={popupOpen ? Close : Message}
           />
         </div>
-        <Input
-          id="feedback-name"
-          label="Name"
-          type="text"
-          name="name"
-          placeholder="Robin"
-          orientation="vertical"
-        />
-        <div style={{ display: "none" }} className="hidden">
-          <Input
-            label="pageUrl"
-            type="text"
-            name="pageUrl"
-            value={pageUrl}
-            placeholder=""
-            orientation="vertical"
-          />
-        </div>
-
-        <Input
-          id="feedback-email"
-          label="Email Address"
-          type="email"
-          name="user_name"
-          placeholder="example@email.co.uk"
-          orientation="vertical"
-        />
-        <TextArea
-          id="feedback-message"
-          label="Feedback"
-          type="email"
-          name="feedback"
-          placeholder=""
-          orientation="vertical"
-          rows="5"
-        />
-        <Button variant="primary" type="submit" label={submitFeedback} />
+        {formSubmitted ? (
+          <>
+            <div style={{ marginTop: "1rem" }}>
+              Thank you for your feedback!
+            </div>
+            <video loop autoPlay muted>
+              <source
+                src="https://cdn.dribbble.com/users/472667/screenshots/15234142/media/cb0e236c6b047295fd672ef10acefba1.mp4"
+                type="video/mp4"
+              />
+            </video>
+          </>
+        ) : (
+          <>
+            <Input
+              id="feedback-name"
+              label="Name"
+              type="text"
+              name="name"
+              placeholder="Robin"
+              orientation="vertical"
+            />
+            <div style={{ display: "none" }} className="hidden">
+              <Input
+                label="pageUrl"
+                type="text"
+                name="pageUrl"
+                value={pageUrl}
+                placeholder=""
+                orientation="vertical"
+              />
+            </div>
+            <Input
+              id="feedback-email"
+              label="Email Address"
+              type="email"
+              name="user_name"
+              placeholder="example@email.co.uk"
+              orientation="vertical"
+            />
+            <TextArea
+              id="feedback-message"
+              label="Feedback"
+              type="email"
+              name="feedback"
+              placeholder=""
+              orientation="vertical"
+              rows="5"
+            />
+            <Button variant="primary" type="submit" label={submitFeedback} />}
+          </>
+        )}
       </form>
     </div>
   );

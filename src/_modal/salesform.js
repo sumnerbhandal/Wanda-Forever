3656e52905eec6429d9a55859d77477497406b92
Sumner Bhandal <34420026+sumnerbhandal@users.noreply.github.com>
2022-06-10 19:21:44 +0100

    Phase 1 Complete

--- src/_modal/salesform.js
+++ src/_modal/salesform.js
@@ -18,6 +18,11 @@ const sendRequest = (
 const SalesForm = () => {
   const form = useRef();
   const [formSubmitted, setFormSubmitted] = useState(false);
+  const [contractSelected, setContractSelected] = useState(false);
+
+  const selectContract = (e) => {
+    setContractSelected(true);
+  };
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -66,32 +71,65 @@ const SalesForm = () => {
                 id="sales-contract-type"
                 value="Latest"
                 orientation="vertical"
-                option={["Version 1", "Original Version"]}
+                option={[
+                  "Please Select",
+                  "Amendment",
+                  "Consulting Agreement Company",
+                  "Consulting Agreement Individual",
+                  "Distribution Agreement",
+                  "End User License Agreement",
+                  "License Agreement",
+                  "Material Transfer Agreement One-Way Academic",
+                  "Material Transfer Agreement One-Way Company",
+                  "Material Transfer Agreement One-Way Prototype",
+                  "Material Transfer Agreement Mutual",
+                  "NDA One-Way",
+                  "NDA Two-Way",
+                  "NDA Three-Way",
+                  "Non-Recurring Engineering Agreement",
+                  "Procurement Agreement",
+                  "Quality Agreement Repligen Supply",
+                  "Quality Agreement Procurement",
+                  "Real Estate Agreement",
+                  "Recruiting Agency Agreement",
+                  "Rental Agreement",
+                  "Research Agreement",
+                  "Service Agreement",
+                  "Strategic Agreement",
+                  "Supply Agreement",
+                  "Other"
+                ]}
                 name="contract_type"
+                onChange={selectContract}
               />
-              <Input
-                label="Counterparty Entity Details*"
-                id="sales-counterparty-entity"
-                placeholder="Details"
-                orientation="vertical"
-                name="counterparty_entity_details"
-              />
-              <DropDown
-                label="Client Entity*"
-                id="sales-client-entity"
-                value="Latest"
-                orientation="vertical"
-                option={["Please Select", "Original Version"]}
-                name="client_entity"
-              />
-              <TextArea
-                label="Transaction Details*"
-                id="sales-transaction-details"
-                placeholder="Transaction details"
-                orientation="vertical"
-                rows="4"
-                name="transaction_details"
-              />
+              {contractSelected ? (
+                <div className="nda-selected">
+                  <hr />
+                  <Input
+                    label="Counterparty Entity Details*"
+                    id="sales-counterparty-entity"
+                    placeholder="Details"
+                    orientation="vertical"
+                    name="counterparty_entity_details"
+                  />
+                  <DropDown
+                    label="Client Entity*"
+                    id="sales-client-entity"
+                    value="Latest"
+                    orientation="vertical"
+                    option={["Please Select", "Client Entity Example"]}
+                    name="client_entity"
+                  />
+                  <TextArea
+                    label="Transaction Details*"
+                    id="sales-transaction-details"
+                    placeholder="Transaction details"
+                    orientation="vertical"
+                    rows="4"
+                    name="transaction_details"
+                  />
+                </div>
+              ) : null}
               <div className="button-container">
                 <Button
                   id="sales=request-discard"
